365833b3ba3dddb327a400f374ea2d6ccec9994b
Timofey Abramov <abramov@i-sensor.ru>
2019-06-09 08:52:09 +0700

    Translations has been updated

--- WebCamRecognizer_ru.ts
+++ WebCamRecognizer_ru.ts
@@ -5,7 +5,7 @@
     <name>MainWindow</name>
     <message>
         <location filename="mainwindow.ui" line="14"/>
-        <location filename="mainwindow.cpp" line="283"/>
+        <location filename="mainwindow.cpp" line="291"/>
         <source>WebCamRecognizer</source>
         <translation>WebCamRecognizer</translation>
     </message>
@@ -94,52 +94,58 @@
         <translation>О Qt</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="129"/>
+        <location filename="mainwindow.cpp" line="135"/>
         <source>Viewfinder [%1]</source>
         <translation>Видеоискатель [%1]</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="214"/>
+        <location filename="mainwindow.cpp" line="151"/>
+        <source>Camera is not available</source>
+        <translation>Камера недоступна</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="220"/>
         <source>Camera Off</source>
         <translation>Выключить камеру</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="228"/>
+        <location filename="mainwindow.cpp" line="235"/>
         <source>Camera On</source>
         <translation>Включить камеру</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="246"/>
+        <location filename="mainwindow.cpp" line="254"/>
         <source>Focus and Lock Camera</source>
         <translation>Сфокусировать</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="257"/>
+        <location filename="mainwindow.cpp" line="265"/>
         <source>Unlock Camera</source>
         <translation>Расфокусировать</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="267"/>
+        <location filename="mainwindow.cpp" line="151"/>
+        <location filename="mainwindow.cpp" line="275"/>
         <source>Camera Error</source>
         <translation>Ошибка камеры</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="284"/>
+        <location filename="mainwindow.cpp" line="292"/>
         <source>Prorgram version: </source>
         <translation>Версия программы: </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="285"/>
+        <location filename="mainwindow.cpp" line="293"/>
         <source>OpenCV version: </source>
         <translation>Версия OpenCV: </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="286"/>
+        <location filename="mainwindow.cpp" line="294"/>
         <source>Qt version: </source>
         <translation>Версия Qt: </translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="287"/>
+        <location filename="mainwindow.cpp" line="295"/>
         <source>Application for chairs recognition and counting on webcam snapshots</source>
         <translation>Программа для распознавания стульев и подсчета их количества на снимках веб-камеры</translation>
     </message>
@@ -147,14 +153,27 @@
 <context>
     <name>MobileNetSSDRecognizer</name>
     <message>
-        <location filename="mobilenetssdrecognizer.cpp" line="35"/>
+        <location filename="mobilenetssdrecognizer.cpp" line="32"/>
         <source>Null image</source>
         <translation>Нет изображения</translation>
     </message>
     <message>
-        <location filename="mobilenetssdrecognizer.cpp" line="44"/>
+        <location filename="mobilenetssdrecognizer.cpp" line="41"/>
         <source>Unsupported image format</source>
         <translation>Неподдерживаемый формат</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="main.cpp" line="14"/>
+        <source>WebCamRecognizer</source>
+        <translation>WebCamRecognizer</translation>
+    </message>
+    <message>
+        <location filename="main.cpp" line="15"/>
+        <source>Timofey Abramov</source>
+        <translation>Тимофей Абрамов</translation>
     </message>
 </context>
 <context>
@@ -165,9 +184,14 @@
         <translation>Ошибка: %1</translation>
     </message>
     <message>
-        <location filename="recognitionitem.cpp" line="47"/>
+        <location filename="recognitionitem.cpp" line="48"/>
         <source>Chair: %1 %</source>
         <translation>Стул: %1 %</translation>
+    </message>
+    <message>
+        <location filename="recognitionitem.cpp" line="55"/>
+        <source>Chairs Count = %1 [Duration = %2 ms]</source>
+        <translation>Кол-во стульев = %1 [длит = %2 мс]</translation>
     </message>
 </context>
 </TS>
